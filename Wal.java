b65df3cb78c384a3ef887bac04e3331f8ff80f39
Darryl Amatsetam <d.amatsetam@gmail.com>
2013-12-11 03:34:52 +0100

    einde sprint 1 minigame 1 upload

---
+++ Wal.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Wal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Wal extends Actor
+{
+    /**
+     * Act - do whatever the Wal wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Wal()
+    {
+        getImage().scale((int) Haven.bloksizeX,(int) Haven.bloksizeY);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ WayPoint.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WayPoint here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WayPoint extends Actor
+{
+    private int wayPointNo;
+
+    public WayPoint() {
+        getImage().scale((int) Haven.bloksizeX,(int) Haven.bloksizeY);
+    }
+
+    public WayPoint(int wayPointNo) {
+        this.wayPointNo = wayPointNo;
+        getImage().scale((int) Haven.bloksizeX,(int) Haven.bloksizeY);
+    }
+
+    /**
+     * Act - do whatever the WayPoint wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        checkShipRequest();
+    }
+
+    private void checkShipRequest() {
+        Boot boot = (Boot) getOneIntersectingObject(Boot.class);
+        if (boot != null && wayPointNo == 0) {
+            boot.moveToNxtPoint();
+        }
+    }
+}
